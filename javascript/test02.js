562c9a792dcee3d64ae25a6fae01525932d28e41
leejeongmin <abcdiue@nate.com>
2024-05-21 10:11:05 +0900

    현재까지 수업했던 html,javascript 예제 추가함

---
+++ javascript/test02.js
@@ -0,0 +1,13 @@
+// test02.js
+/*
+    html 요소에 일어나는 이벤트
+
+    클릭하면 => click
+    마우스의 커서를 올리면 => mouseover
+    마우스 커서가 요소를 벗어나면 => mosueout
+    마우스를 클릭하면 => mousedown
+    마우스를 클릭후 때면 => mouseup
+    포커스가 오면 => focus
+    포커를 잃으면 => blur ... 등등
+*/
+
